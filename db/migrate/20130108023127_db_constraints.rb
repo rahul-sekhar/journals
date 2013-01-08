f13177522215d417bf85245d87885c839d07cae0
Rahul Sekhar <rahul@kairi.in>
2013-01-08 08:55:12 +0530

    Added db constraints

---
+++ db/migrate/20130108023127_db_constraints.rb
@@ -0,0 +1,227 @@
+class DbConstraints < ActiveRecord::Migration
+  def up
+    drop_table :basic_skills
+
+    # Comments
+    change_column :comments, :post_id, :integer, null: false
+    change_column :comments, :author_id, :integer, null: false
+    change_column :comments, :author_type, :string, null: false, limit: 10
+
+    add_index :comments, :created_at
+
+    # Groups
+    change_column :groups, :name, :string, null: false, limit: 50
+    
+    add_index :groups, :name, unique: true
+
+    # Guardians
+    change_column :guardians, :first_name, :string, limit: 80
+    change_column :guardians, :last_name, :string, null: false, limit: 80
+    change_column :guardians, :mobile, :string, limit: 40
+    change_column :guardians, :home_phone, :string, limit: 40
+    change_column :guardians, :office_phone, :string, limit: 40
+
+    add_index :guardians, [:last_name, :first_name]
+
+    # Images
+    change_column :images, :post_id, :integer, null: false
+
+    # Posts
+    change_column :posts, :title, :string, null: false
+    remove_column :posts, :last_commented_at
+    change_column :posts, :author_id, :integer, null: false
+    change_column :posts, :author_type, :string, null: false, limit: 10
+
+    add_index :posts, [:author_type, :author_id]
+    add_index :posts, :title
+
+    # Posts students
+    change_column :posts_students, :post_id, :integer, null: false
+    change_column :posts_students, :student_id, :integer, null: false
+
+    # Posts tags
+    change_column :posts_tags, :post_id, :integer, null: false
+    change_column :posts_tags, :tag_id, :integer, null: false
+
+    # Posts teachers
+    change_column :posts_teachers, :post_id, :integer, null: false
+    change_column :posts_teachers, :teacher_id, :integer, null: false
+
+    # Student mentors
+    change_column :student_mentors, :student_id, :integer, null: false
+    change_column :student_mentors, :teacher_id, :integer, null: false
+
+    # Student observations
+    change_column :student_observations, :student_id, :integer, null: false
+    change_column :student_observations, :post_id, :integer, null: false
+    change_column :student_observations, :content, :text, null: false
+
+    # Students
+    change_column :students, :first_name, :string, limit: 80
+    change_column :students, :last_name, :string, null: false, limit: 80
+    change_column :students, :mobile, :string, limit: 40
+    change_column :students, :home_phone, :string, limit: 40
+    change_column :students, :office_phone, :string, limit: 40
+    change_column :students, :bloodgroup, :string, limit: 15
+    change_column :students, :archived, :boolean, null: false, default: false
+
+    remove_index :students, name: :index_student_profiles_on_first_name
+    remove_index :students, name: :index_student_profiles_on_last_name
+
+    # Students groups
+    change_column :students_groups, :student_id, :integer, null: false
+    change_column :students_groups, :group_id, :integer, null: false
+
+    # Students groups
+    change_column :students_guardians, :student_id, :integer, null: false
+    change_column :students_guardians, :guardian_id, :integer, null: false
+    
+    add_index :students_guardians, [:student_id, :guardian_id], unique: true
+    add_index :students_guardians, :guardian_id
+
+    # Tags
+    change_column :tags, :name, :string, null: false, limit: 50
+
+    remove_index :tags, name: :index_subjects_on_name
+    add_index :tags, :name, unique: true
+
+    # Teachers
+    change_column :teachers, :first_name, :string, limit: 80
+    change_column :teachers, :last_name, :string, null: false, limit: 80
+    change_column :teachers, :mobile, :string, limit: 40
+    change_column :teachers, :home_phone, :string, limit: 40
+    change_column :teachers, :office_phone, :string, limit: 40
+    change_column :teachers, :archived, :boolean, null: false, default: false
+
+    remove_index :teachers, name: :index_teacher_profiles_on_first_name
+    remove_index :teachers, name: :index_teacher_profiles_on_last_name
+
+    # Users
+    change_column :users, :email, :string, null: false, limit: 60
+    change_column :users, :password_hash, :string, limit: 60
+    change_column :users, :password_salt, :string, null: false, limit: 60
+    change_column :users, :profile_id, :integer, null: false
+    change_column :users, :profile_type, :string, null: false, limit: 10
+
+    remove_index :users, name: :index_users_on_username
+    remove_index :users, name: :users_profile_index
+    add_index :users, :email, unique: true
+    add_index :users, [:profile_type, :profile_id], unique: true
+  end
+
+  def down
+    create_table :basic_skills do |t|
+      t.integer :subject_id
+      t.timestamps
+    end
+
+    # Comments
+    change_column :comments, :post_id, :integer, null: true
+    change_column :comments, :author_id, :integer, null: true
+    change_column :comments, :author_type, :string, null: true, limit: 10
+
+    remove_index :comments, :created_at
+
+    # Groups
+    change_column :groups, :name, :string, null: true
+    
+    remove_index :groups, :name
+
+    # Guardians
+    change_column :guardians, :first_name, :string
+    change_column :guardians, :last_name, :string, null: true
+    change_column :guardians, :mobile, :string
+    change_column :guardians, :home_phone, :string
+    change_column :guardians, :office_phone, :string
+
+    remove_index :guardians, [:last_name, :first_name]
+
+    # Images
+    change_column :images, :post_id, :integer, null: true
+
+    # Posts
+    change_column :posts, :title, :string, null: true
+    add_column :posts, :last_commented_at, :datetime
+    change_column :posts, :author_id, :integer, null: true
+    change_column :posts, :author_type, :string, null: true
+
+    add_index :posts, :last_commented_at
+    remove_index :posts, [:author_type, :author_id]
+    remove_index :posts, :title
+
+    # Posts students
+    change_column :posts_students, :post_id, :integer, null: true
+    change_column :posts_students, :student_id, :integer, null: true
+
+    # Posts tags
+    change_column :posts_tags, :post_id, :integer, null: true
+    change_column :posts_tags, :tag_id, :integer, null: true
+
+    # Posts teachers
+    change_column :posts_teachers, :post_id, :integer, null: true
+    change_column :posts_teachers, :teacher_id, :integer, null: true
+
+    # Student mentors
+    change_column :student_mentors, :student_id, :integer, null: true
+    change_column :student_mentors, :teacher_id, :integer, null: true
+
+    # Student observations
+    change_column :student_observations, :student_id, :integer, null: true
+    change_column :student_observations, :post_id, :integer, null: true
+    change_column :student_observations, :content, :text, null: true
+
+    # Students
+    change_column :students, :first_name, :string
+    change_column :students, :last_name, :string, null: true
+    change_column :students, :mobile, :string
+    change_column :students, :home_phone, :string
+    change_column :students, :office_phone, :string
+    change_column :students, :bloodgroup, :string
+    change_column :students, :archived, :boolean, null: true
+    change_column_default :students, :archived, nil
+
+    add_index :students, :first_name, name: :index_student_profiles_on_first_name
+    add_index :students, :last_name, name: :index_student_profiles_on_last_name
+
+    # Students groups
+    change_column :students_groups, :student_id, :integer, null: true
+    change_column :students_groups, :group_id, :integer, null: true
+
+    # Students groups
+    change_column :students_guardians, :student_id, :integer, null: true
+    change_column :students_guardians, :guardian_id, :integer, null: true
+    
+    remove_index :students_guardians, [:student_id, :guardian_id]
+    remove_index :students_guardians, :guardian_id
+
+    # Tags
+    change_column :tags, :name, :string, null: true, limit: 50
+
+    remove_index :tags, :name
+    add_index :tags, :name, name: :index_subjects_on_name
+
+    # Teachers
+    change_column :teachers, :first_name, :string
+    change_column :teachers, :last_name, :string, null: true
+    change_column :teachers, :mobile, :string
+    change_column :teachers, :home_phone, :string
+    change_column :teachers, :office_phone, :string
+    change_column :teachers, :archived, :boolean, null: true
+    change_column_default :teachers, :archived, nil
+
+    add_index :teachers, :first_name, name: :index_teacher_profiles_on_first_name
+    add_index :teachers, :last_name, name: :index_teacher_profiles_on_last_name
+
+    # Users
+    change_column :users, :email, :string, null: true
+    change_column :users, :password_hash, :string
+    change_column :users, :password_salt, :string, null: true
+    change_column :users, :profile_id, :integer, null: true
+    change_column :users, :profile_type, :string, null: true
+
+    remove_index :users, :email
+    remove_index :users, [:profile_type, :profile_id]
+    add_index :users, :email, name: :index_users_on_username
+    add_index :users, [:profile_id, :profile_type], name: :users_profile_index
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130107173643) do
+ActiveRecord::Schema.define(:version => 20130108023127) do
 
   create_table "academics", :force => true do |t|
     t.string   "name"
@@ -19,24 +19,17 @@ ActiveRecord::Schema.define(:version => 20130107173643) do
     t.datetime "updated_at"
   end
 
-  create_table "basic_skills", :force => true do |t|
-    t.integer  "subject_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "basic_skills", ["subject_id"], :name => "index_basic_skills_on_subject_id", :unique => true
-
   create_table "comments", :force => true do |t|
     t.text     "content"
-    t.integer  "post_id"
+    t.integer  "post_id",                   :null => false
     t.datetime "created_at"
     t.datetime "updated_at"
     t.datetime "edited_at"
-    t.integer  "author_id"
-    t.string   "author_type"
+    t.integer  "author_id",                 :null => false
+    t.string   "author_type", :limit => 10, :null => false
   end
 
+  add_index "comments", ["created_at"], :name => "index_comments_on_created_at"
   add_index "comments", ["post_id"], :name => "index_comments_on_post_id"
 
   create_table "delayed_jobs", :force => true do |t|
@@ -56,25 +49,29 @@ ActiveRecord::Schema.define(:version => 20130107173643) do
   add_index "delayed_jobs", ["priority", "run_at"], :name => "delayed_jobs_priority"
 
   create_table "groups", :force => true do |t|
-    t.string   "name"
+    t.string   "name",       :limit => 50, :null => false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
+  add_index "groups", ["name"], :name => "index_groups_on_name", :unique => true
+
   create_table "guardians", :force => true do |t|
-    t.string   "first_name"
-    t.string   "last_name"
-    t.string   "mobile"
+    t.string   "first_name",   :limit => 80
+    t.string   "last_name",    :limit => 80, :null => false
+    t.string   "mobile",       :limit => 40
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "home_phone"
-    t.string   "office_phone"
+    t.string   "home_phone",   :limit => 40
+    t.string   "office_phone", :limit => 40
     t.text     "address"
   end
 
+  add_index "guardians", ["last_name", "first_name"], :name => "index_guardians_on_last_name_and_first_name"
+
   create_table "images", :force => true do |t|
     t.string   "file_name"
-    t.integer  "post_id"
+    t.integer  "post_id",    :null => false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -93,40 +90,40 @@ ActiveRecord::Schema.define(:version => 20130107173643) do
   add_index "milestones", ["strand_id"], :name => "index_milestones_on_strand_id"
 
   create_table "posts", :force => true do |t|
-    t.string   "title"
+    t.string   "title",                                                 :null => false
     t.text     "content"
-    t.boolean  "visible_to_students",  :default => false, :null => false
-    t.boolean  "visible_to_guardians", :default => false, :null => false
+    t.boolean  "visible_to_students",                :default => false, :null => false
+    t.boolean  "visible_to_guardians",               :default => false, :null => false
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.datetime "last_commented_at"
     t.datetime "edited_at"
-    t.integer  "author_id"
-    t.string   "author_type"
+    t.integer  "author_id",                                             :null => false
+    t.string   "author_type",          :limit => 10,                    :null => false
   end
 
+  add_index "posts", ["author_type", "author_id"], :name => "index_posts_on_author_type_and_author_id"
   add_index "posts", ["created_at"], :name => "index_posts_on_created_at"
-  add_index "posts", ["last_commented_at"], :name => "index_posts_on_last_commented_at"
+  add_index "posts", ["title"], :name => "index_posts_on_title"
   add_index "posts", ["visible_to_guardians"], :name => "index_posts_on_guardians_restricted"
   add_index "posts", ["visible_to_students"], :name => "index_posts_on_students_restricted"
 
   create_table "posts_students", :id => false, :force => true do |t|
-    t.integer "post_id"
-    t.integer "student_id"
+    t.integer "post_id",    :null => false
+    t.integer "student_id", :null => false
   end
 
   add_index "posts_students", ["post_id", "student_id"], :name => "index_posts_tagged_students_on_post_id_and_student_profile_id", :unique => true
 
   create_table "posts_tags", :id => false, :force => true do |t|
-    t.integer "post_id"
-    t.integer "tag_id"
+    t.integer "post_id", :null => false
+    t.integer "tag_id",  :null => false
   end
 
   add_index "posts_tags", ["post_id", "tag_id"], :name => "index_posts_subjects_on_post_id_and_subject_id", :unique => true
 
   create_table "posts_teachers", :id => false, :force => true do |t|
-    t.integer "post_id"
-    t.integer "teacher_id"
+    t.integer "post_id",    :null => false
+    t.integer "teacher_id", :null => false
   end
 
   add_index "posts_teachers", ["post_id", "teacher_id"], :name => "index_posts_teachers_on_post_id_and_teacher_profile_id", :unique => true
@@ -143,16 +140,16 @@ ActiveRecord::Schema.define(:version => 20130107173643) do
   add_index "strands", ["parent_strand_id"], :name => "index_strands_on_parent_strand_id"
 
   create_table "student_mentors", :id => false, :force => true do |t|
-    t.integer "student_id"
-    t.integer "teacher_id"
+    t.integer "student_id", :null => false
+    t.integer "teacher_id", :null => false
   end
 
   add_index "student_mentors", ["student_id", "teacher_id"], :name => "students_mentors_index", :unique => true
 
   create_table "student_observations", :force => true do |t|
-    t.text     "content"
-    t.integer  "post_id"
-    t.integer  "student_id"
+    t.text     "content",    :null => false
+    t.integer  "post_id",    :null => false
+    t.integer  "student_id", :null => false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -160,35 +157,36 @@ ActiveRecord::Schema.define(:version => 20130107173643) do
   add_index "student_observations", ["post_id", "student_id"], :name => "index_post_sections_on_post_id_and_student_profile_id"
 
   create_table "students", :force => true do |t|
-    t.string   "first_name"
-    t.string   "last_name"
+    t.string   "first_name",            :limit => 80
+    t.string   "last_name",             :limit => 80,                    :null => false
     t.datetime "created_at"
     t.datetime "updated_at"
     t.date     "birthday"
-    t.string   "bloodgroup"
+    t.string   "bloodgroup",            :limit => 15
     t.text     "address"
     t.string   "application_form_file"
-    t.string   "home_phone"
-    t.string   "mobile"
-    t.string   "office_phone"
-    t.boolean  "archived"
+    t.string   "home_phone",            :limit => 40
+    t.string   "mobile",                :limit => 40
+    t.string   "office_phone",          :limit => 40
+    t.boolean  "archived",                            :default => false, :null => false
   end
 
   add_index "students", ["first_name", "last_name"], :name => "student_full_name_index"
-  add_index "students", ["first_name"], :name => "index_student_profiles_on_first_name"
-  add_index "students", ["last_name"], :name => "index_student_profiles_on_last_name"
 
   create_table "students_groups", :id => false, :force => true do |t|
-    t.integer "student_id"
-    t.integer "group_id"
+    t.integer "student_id", :null => false
+    t.integer "group_id",   :null => false
   end
 
   add_index "students_groups", ["student_id", "group_id"], :name => "students_groups_index", :unique => true
 
   create_table "students_guardians", :id => false, :force => true do |t|
-    t.integer "student_id"
-    t.integer "guardian_id"
+    t.integer "student_id",  :null => false
+    t.integer "guardian_id", :null => false
   end
+
+  add_index "students_guardians", ["guardian_id"], :name => "index_students_guardians_on_guardian_id"
+  add_index "students_guardians", ["student_id", "guardian_id"], :name => "index_students_guardians_on_student_id_and_guardian_id", :unique => true
 
   create_table "students_milestones", :force => true do |t|
     t.integer  "student_id"
@@ -202,12 +200,12 @@ ActiveRecord::Schema.define(:version => 20130107173643) do
   add_index "students_milestones", ["student_id", "milestone_id"], :name => "students_milestones_index"
 
   create_table "tags", :force => true do |t|
-    t.string   "name"
+    t.string   "name",       :limit => 50, :null => false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  add_index "tags", ["name"], :name => "index_subjects_on_name"
+  add_index "tags", ["name"], :name => "index_tags_on_name", :unique => true
 
   create_table "teacher_academic_students", :id => false, :force => true do |t|
     t.integer  "teacher_academic_id"
@@ -228,20 +226,18 @@ ActiveRecord::Schema.define(:version => 20130107173643) do
   add_index "teacher_academics", ["academic_id", "teacher_id"], :name => "academics_teachers_index"
 
   create_table "teachers", :force => true do |t|
-    t.string   "first_name"
-    t.string   "last_name"
+    t.string   "first_name",   :limit => 80
+    t.string   "last_name",    :limit => 80,                    :null => false
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "mobile"
+    t.string   "mobile",       :limit => 40
     t.text     "address"
-    t.string   "home_phone"
-    t.string   "office_phone"
-    t.boolean  "archived"
+    t.string   "home_phone",   :limit => 40
+    t.string   "office_phone", :limit => 40
+    t.boolean  "archived",                   :default => false, :null => false
   end
 
   add_index "teachers", ["first_name", "last_name"], :name => "teacher_full_name_index"
-  add_index "teachers", ["first_name"], :name => "index_teacher_profiles_on_first_name"
-  add_index "teachers", ["last_name"], :name => "index_teacher_profiles_on_last_name"
 
   create_table "units", :force => true do |t|
     t.integer  "student_id"
@@ -258,16 +254,16 @@ ActiveRecord::Schema.define(:version => 20130107173643) do
   add_index "units", ["student_id", "academic_id"], :name => "student_units_index"
 
   create_table "users", :force => true do |t|
-    t.string   "email"
-    t.string   "password_hash"
-    t.string   "password_salt"
-    t.integer  "profile_id"
-    t.string   "profile_type"
+    t.string   "email",         :limit => 60, :null => false
+    t.string   "password_hash", :limit => 60
+    t.string   "password_salt", :limit => 60, :null => false
+    t.integer  "profile_id",                  :null => false
+    t.string   "profile_type",  :limit => 10, :null => false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  add_index "users", ["email"], :name => "index_users_on_username"
-  add_index "users", ["profile_id", "profile_type"], :name => "users_profile_index"
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+  add_index "users", ["profile_type", "profile_id"], :name => "index_users_on_profile_type_and_profile_id", :unique => true
 
 end
