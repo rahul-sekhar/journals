edc763ce80a903d3c5f3afe7a599500139114c53
Rahul Sekhar <sekhar.rahul@gmail.com>
2013-02-07 13:48:00 +0530

    Made secret token private

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Journals::Application.config.secret_token = '3956c8e0dee48c47d35c63a8eb9b1e89ba540c870821c4798d2517360499b25617b5764f394bcc1c933741ec8b8f4bcdeb2f1e9aaf28664ecb06202f2ac1fd75'
+Journals::Application.config.secret_token = Rails.configuration.sensitive['secret_token']
