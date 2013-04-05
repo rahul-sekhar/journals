4bf159a4f5d0eefbc43733fc432537bbf3df143c
Rahul Sekhar <sekhar.rahul@gmail.com>
2013-04-05 19:14:57 +0530

    link dialog

--- app/assets/javascripts/angular/tinyMCE.js
+++ app/assets/javascripts/angular/tinyMCE.js
@@ -2,7 +2,7 @@
 
 angular.module('journals.tinymce', ['journals.fileUpload']).
 
-  directive('tinymce', ['imageDialog', function (imageDialog) {
+  directive('tinymce', ['imageDialog', 'linkDialog', function (imageDialog, linkDialog) {
     tinyMCE.baseURL = '/tinymce/';
 
     return {
@@ -14,9 +14,9 @@ angular.module('journals.tinymce', ['journals.fileUpload']).
           popup_css : "/tinymce/themes/advanced/skins/default/dialog.css",
           width: 474,
           theme : "advanced",
-          plugins : "autolink,lists,inlinepopups,paste,-ngimage",
+          plugins : "autolink,lists,inlinepopups,paste,-ngimage,-nglink",
           content_css: "/assets/tinymce/main.css",
-          theme_advanced_buttons1 : "bold,italic,strikethrough,|,link,unlink,|,bullist,numlist,|,ngimage",
+          theme_advanced_buttons1 : "bold,italic,strikethrough,|,nglink,unlink,|,bullist,numlist,|,ngimage,|,code",
           theme_advanced_toolbar_location : "top",
           theme_advanced_toolbar_align : "center",
           theme_advanced_resizing : true,
@@ -54,7 +54,7 @@ angular.module('journals.tinymce', ['journals.fileUpload']).
     var modal, scope, currentEditor;
 
     // Set up dialog
-    modal = angular.element('<modal id="image-upload-dialog" show-on="imageDialogShown">' +
+    modal = angular.element('<modal id="image-upload-dialog" show-on="dialogShown">' +
       '<p>Upload an image</p>' +
       '<file-upload url="/images.json" on-upload="uploaded(imageData)"></file-upload>' +
       '<button ng-click="cancel()">Cancel</button>' +
@@ -65,17 +65,18 @@ angular.module('journals.tinymce', ['journals.fileUpload']).
     modal = $compile(modal)(scope);
 
     scope.cancel = function () {
-      scope.imageDialogShown = false;
+      scope.dialogShown = false;
     };
 
-    scope.$watch('imageDialogShown', function (value) {
+    scope.$watch('dialogShown', function (value) {
       if (value === false) {
         scope.$broadcast('cancelUpload');
+        currentEditor.focus();
       }
     });
 
     scope.uploaded = function (imageData) {
-      scope.imageDialogShown = false;
+      scope.dialogShown = false;
       currentEditor.execCommand('mceInsertContent', false, '<img src="' + imageData.url + '" alt="" />');
       $rootScope.$broadcast('imageUploaded', imageData);
     };
@@ -86,7 +87,9 @@ angular.module('journals.tinymce', ['journals.fileUpload']).
         editor.addCommand('ngImage', function() {
           scope.$broadcast('resetUpload');
           currentEditor = editor;
-          scope.imageDialogShown = true;
+          scope.$apply(function () {
+            scope.dialogShown = true;
+          });
         });
 
         editor.addButton('ngimage', {
@@ -98,4 +101,82 @@ angular.module('journals.tinymce', ['journals.fileUpload']).
     });
 
     tinymce.PluginManager.add('ngimage', tinymce.plugins.ngImage);
+  }]).
+
+  factory('linkDialog', ['$compile', '$rootScope', function ($compile, $rootScope) {
+    var modal, scope, currentEditor;
+
+    // Set up dialog
+    modal = angular.element('<modal id="link-dialog" show-on="dialogShown">' +
+      '<div class="inputs">' +
+        '<label for="link-url">{{linkSelected && "Add a link with the URL:" || "Edit link URL:"}}</label>' +
+        '<input ng-model="link.url" id="link-url" />' +
+      '</div>' +
+      '<button ng-click="submit(link.url)">Ok</button>' +
+      '<button ng-click="cancel()">Cancel</button>' +
+    '</modal>');
+
+    modal.appendTo('body')
+    scope = $rootScope.$new();
+    modal = $compile(modal)(scope);
+
+    scope.link = {};
+
+    scope.cancel = function () {
+      scope.dialogShown = false;
+    };
+
+    scope.submit = function (url) {
+      scope.dialogShown = false;
+
+      if (url) {
+        if (url.search('http') === -1) {
+          url = 'http://' + url;
+        }
+
+        if (currentEditor.selection.getContent() || currentEditor.selection.getNode().nodeName === 'A') {
+          currentEditor.execCommand('mceInsertLink', false, url);
+        } else {
+          currentEditor.execCommand('mceInsertContent', false, '<a href="' + url + '">' + url + '</a>');
+        }
+      }
+    };
+
+    scope.$watch('dialogShown', function (value) {
+      if (value === false) {
+        currentEditor.focus();
+      }
+    });
+
+    // Set up plugin
+    tinymce.create('tinymce.plugins.ngLink', {
+      init: function (editor, url) {
+        editor.addCommand('ngLink', function() {
+          var selectedNode;
+
+          currentEditor = editor;
+
+          selectedNode = currentEditor.selection.getNode();
+          scope.$apply(function () {
+            if (selectedNode.nodeName === 'A') {
+              scope.linkSelected = true;
+              scope.link.url = $(selectedNode).attr('href');
+            } else {
+              scope.linkSelected = false;
+              scope.link.url = '';
+            }
+
+            scope.dialogShown = true;
+          });
+        });
+
+        editor.addButton('nglink', {
+          title : 'Insert/Edit link',
+          cmd : 'ngLink',
+          class: 'mce_link'
+        });
+      }
+    });
+
+    tinymce.PluginManager.add('nglink', tinymce.plugins.ngLink);
   }]);
