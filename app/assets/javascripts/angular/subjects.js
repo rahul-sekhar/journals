5938005f22422cfd9c90e38abb224e7b2e40ab99
Rahul Sekhar <sekhar.rahul@gmail.com>
2014-02-22 13:31:02 +0530

    bug fixes

--- app/assets/javascripts/angular/subjects.js
+++ app/assets/javascripts/angular/subjects.js
@@ -13,6 +13,11 @@ angular.module('journals.subjects', ['journals.ajax', 'journals.collection', 'jo
   factory('Framework', ['model', 'association', 'Milestones', 'StudentMilestone',
     function (model, association, Milestones, StudentMilestone) {
       var frameworkExtension = function (instance) {
+        // Clear old student milestones
+        angular.forEach(Milestones.all(), function (milestone) {
+          milestone.student_milestone = StudentMilestone.create({milestone_id: milestone.id});
+        });
+
         if (instance.student_milestones) {
           angular.forEach(instance.student_milestones, function (studentMilestone) {
             var milestone = Milestones.get(parseInt(studentMilestone.milestone_id, 10));
@@ -86,11 +91,7 @@ angular.module('journals.subjects', ['journals.ajax', 'journals.collection', 'jo
 
   factory('Milestones', ['collection', 'model', 'StudentMilestone',
     function (collection, model, StudentMilestone) {
-      var milestoneExtension = function (instance) {
-        if (!instance.student_milestone) {
-          instance.student_milestone = StudentMilestone.create({milestone_id: instance.id});
-        }
-      };
+      var milestoneExtension = function (instance) {};
 
       milestoneExtension.setup = function (instance) {
         var oldUrlFn = instance.url;
